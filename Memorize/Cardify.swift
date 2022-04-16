6b74484c079cfd2d0b07949148e9353b76accd56
Marcos Lara <marcos.andre.dev@outlook.com>
2022-04-16 15:22:07 -0300

    vs 0.006 - Aula 06 - ViewModifier Animation

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,39 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Marcos André Novaes de Lara on 16/04/22.
+//
+// Quase sempre que criamos um ViewModifier, criamos uma pequena extensão para o protocolo View, que define uma função para fazer com que o nosso ViewModifier, o modifique
+// Todas as animações acontecem dentro de ViewModifiers ou Shapes
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+  var isFaceUp: Bool
+  
+  func body(content: Content) -> some View {
+    ZStack {
+      let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+      if isFaceUp {
+        shape.fill().foregroundColor(.white)
+        shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+      }
+      else {
+        shape.fill()
+      }
+      content.opacity(isFaceUp ? 1 : 0)
+    }
+  }
+  
+  private struct DrawingConstants {
+    static let cornerRadius: CGFloat = 10
+    static let lineWidth: CGFloat = 3
+  }
+}
+
+extension View {
+  func cardify(isFaceUp: Bool) -> some View {
+    self.modifier(Cardify(isFaceUp: isFaceUp))
+  }
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -34,33 +34,27 @@ struct CardView: View {
   var body: some View {
     GeometryReader{
       geometry in
-      ZStack {
-        let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-        if card.isFaceUp {
-          shape.fill().foregroundColor(.white)
-          shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-          Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 360-90)).padding(DrawingConstants.circlePadding).opacity(DrawingConstants.circleOpacity)
-          Text(card.content).font(font(in: geometry.size))
-        } else if card.isMatched {
-          shape.opacity(0)
-        }
-        else {
-          shape.fill()
-        }
+      ZStack{
+        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(DrawingConstants.circlePadding).opacity(DrawingConstants.circleOpacity)
+        Text(card.content)
+          .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+          .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+          .font(Font.system(size: DrawingConstants.fontSize))
+          .scaleEffect(scale(thatFits: geometry.size))
       }
+      .cardify(isFaceUp: card.isFaceUp)
     }
   }
   
-  private func font(in size: CGSize) -> Font {
-    Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+  private func scale(thatFits size: CGSize) -> CGFloat {
+    min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
   }
   
-    private struct DrawingConstants {
-      static let cornerRadius: CGFloat = 10
-      static let lineWidth: CGFloat = 3
-      static let fontScale: CGFloat = 0.7
-      static let circlePadding: CGFloat = 5
-      static let circleOpacity: CGFloat = 0.5
+  private struct DrawingConstants {
+    static let fontScale: CGFloat = 0.7
+    static let circlePadding: CGFloat = 5
+    static let circleOpacity: CGFloat = 0.5
+    static let fontSize: CGFloat = 32
   }
 }
 
